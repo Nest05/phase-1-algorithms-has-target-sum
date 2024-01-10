16ebcc7764bbff3d2b4b6892c04e90c03a11ecc0
Nestor Masinde <nestormasinde@gmail.com>
2024-01-10 20:27:59 +0300

    Algorithms Has Target Sum

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
-
+//   loop through each number in the array
+  for (let i =0; i < array.length; i++){
+//   loop through the rest of the numbers
+    for (let j = i + 1; j < array.length; j++){
+//     if sum of the current pair equals the target
+      if (array[i] + array[j] === target){
+        return true;
+      }
+//       return true
+    }
+  }
+  return false;
+//  else return false   
+  }
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  loop through each number in the array
+    loop through the rest of the numbers
+      if sum of the current pair equals the target
+        return true
+
+   else return false     
 */
 
 /*
   Add written explanation of your solution here
+  Go through eeach number in the array to check if any 2 numbers add up to the target number.
+  Then, return true if found
 */
 
 // You can run `node index.js` to view these console logs
